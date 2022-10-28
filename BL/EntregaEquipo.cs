def5b54d4097833435821fd756218881b5814dbd
CrMJuarez <cmanueljuarez1@gmail.com>
2022-10-28 11:45:40 -0500

    act

--- BL/EntregaEquipo.cs
+++ BL/EntregaEquipo.cs
@@ -14,27 +14,35 @@ namespace BL
             {
                 using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnectionString("ConnectionStrings:DefaultConnection")))
                 {
-                    string query = "EntregaAdd";
+
+                    string query = "EntregaEquipoAdd";
 
                     SqlCommand cmd = new SqlCommand();
                     cmd.CommandText = query;
                     cmd.Connection = context;
                     cmd.CommandType = CommandType.StoredProcedure;
-                    SqlParameter[] collection = new SqlParameter[1];
+                    SqlParameter[] collection = new SqlParameter[7];
 
-                    collection[0] = new SqlParameter("@Nombre", SqlDbType.VarChar);
-                    collection[0].Value = entregaEquipo.direccionDestino.IdDireccionDestino;
+                    collection[0] = new SqlParameter("@Recibe", SqlDbType.VarChar);
+                    collection[0].Value = entregaEquipo.Recibe;
 
+                    collection[1] = new SqlParameter("@Justificacion", SqlDbType.VarChar);
+                    collection[1].Value = entregaEquipo.Justificacion;
 
+                    collection[2] = new SqlParameter("@IdDireccionDestino", SqlDbType.Int);
+                    collection[2].Value = entregaEquipo.direccionDestino.IdDireccionDestino;
 
+                    collection[3] = new SqlParameter("@IdPersonalEntrega", SqlDbType.Int);
+                    collection[3].Value = entregaEquipo.personalEntrega.IdPersonalEntrega;
 
+                    collection[4] = new SqlParameter("@IdOperadora", SqlDbType.Int);
+                    collection[4].Value = entregaEquipo.operadora.IdOperadora;
 
+                    collection[5] = new SqlParameter("@IdInventario", SqlDbType.Int);
+                    collection[5].Value = entregaEquipo.inventario.IdInventario;
 
-
-
-
-
-
+                    collection[6] = new SqlParameter("@IdPersonalAutorizacion", SqlDbType.Int);
+                    collection[6].Value = entregaEquipo.personalAutorizacion.IdPersonalAutorizacion;                   
 
                     cmd.Parameters.AddRange(collection);
                     cmd.Connection.Open();
@@ -57,6 +65,93 @@ namespace BL
                 result.Ex = ex;
             }
             return result;
+
+        }
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (SqlConnection context = new SqlConnection(DL.Conexion.GetConnectionString("ConnectionStrings:DefaultConnection")))
+                {
+                    string query = "EntregaEquipoGetAll";
+
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = query;
+                    cmd.Connection = context;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection.Open();
+                    DataTable entregaEquipoTable = new DataTable();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(entregaEquipoTable);
+
+                    if (entregaEquipoTable.Rows.Count > 0)
+                    {
+                        result.Objects = new List<object>();
+
+                        foreach (DataRow row in entregaEquipoTable.Rows)
+                        {
+                            ML.EntregaEquipo entregaEquipo = new ML.EntregaEquipo();
+                            entregaEquipo.IdEntregaEquipo = int.Parse(row[0].ToString());
+                            entregaEquipo.Recibe = row[1].ToString();
+                            entregaEquipo.Justificacion = row[2].ToString();
+
+
+                            entregaEquipo.direccionDestino = new ML.DireccionDestino();
+                            entregaEquipo.direccionDestino.IdDireccionDestino = int.Parse(row[3].ToString());
+                            entregaEquipo.direccionDestino.Nombre = row[4].ToString();
+
+
+                            entregaEquipo.personalEntrega = new ML.PersonalEntrega();
+                            entregaEquipo.personalEntrega.IdPersonalEntrega = int.Parse(row[5].ToString());
+                            entregaEquipo.personalEntrega.Nombre = row[6].ToString();
+                            entregaEquipo.personalEntrega.ApellidoPaterno = row[7].ToString();
+                            entregaEquipo.personalEntrega.ApellidoMaterno = row[8].ToString();
+
+
+                            entregaEquipo.personalAutorizacion = new ML.PersonalAutorizacion();
+                            entregaEquipo.personalAutorizacion.IdPersonalAutorizacion = int.Parse(row[9].ToString());
+                            entregaEquipo.personalAutorizacion.Nombre = row[10].ToString();
+                            entregaEquipo.personalAutorizacion.ApellidoPaterno = row[11].ToString();
+                            entregaEquipo.personalAutorizacion.ApellidoMaterno = row[12].ToString();
+
+
+
+                            entregaEquipo.operadora = new ML.Operadora();
+                            entregaEquipo.operadora.IdOperadora = int.Parse(row[13].ToString());
+                            entregaEquipo.operadora.NombreCorto = row[14].ToString();
+
+
+                            entregaEquipo.inventario = new ML.Inventario();
+                            entregaEquipo.inventario.IdInventario = int.Parse(row[15].ToString());
+                            entregaEquipo.inventario.NumeroSerie = row[16].ToString();
+                            result.Objects.Add(entregaEquipo);
+
+
+                        }
+                        result.Correct = true;
+                    }
+                    else
+
+                    {
+                        result.Correct = false;
+                        result.ErrorMessage = "Ocurrió un error al momento de mostrar el equipo de entrega";
+                    }
+                    cmd.Connection.Close();
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = ex.Message;
+                result.Ex = ex;
+
+            }
+            return result;
+
 
         }
     }
--- PL/Controllers/EntregaEquipoController.cs
+++ PL/Controllers/EntregaEquipoController.cs
@@ -5,35 +5,19 @@ namespace PL.Controllers
 {
     public class EntregaEquipoController : Controller
     {
-        public ActionResult GetAll()
+        //public ActionResult GetAll()
+        //{
+        //    ML.EntregaEquipo entregaEquipo = new ML.EntregaEquipo();
+
+        //    return View(entregaEquipo);
+
+        //}
+        public ActionResult Form(int? IdEntregaEquipo)
         {
             ML.EntregaEquipo entregaEquipo = new ML.EntregaEquipo();
-            // ML.Result result = BL.Inventario.GetAll();
-            //ML.Result resultRol = BL.Rol.GetAll();
-            // usuario.Rol = new ML.Rol();
 
-
-            // entregaEquipo.Inventarios = result.Objects.ToList();
             return View(entregaEquipo);
-
-
-        }
-        public ActionResult Form(int? IdInventario)
-        {
-            ML.EntregaEquipo entregaEquipo = new ML.EntregaEquipo();
-            //ML.Result resultRol = BL.Rol.GetAll();
-
-            //usuario.Rol = new ML.Rol();
-            //Tipo equipo
-            //marca
-            //modelo
-            //direccionentrda
-
-            //usuario = (ML.Usuario)result.Object;
-            //usuario.Rol = new ML.Rol();
-            // usuario.Rol.Roles = resultRol.Objects.ToList();
-            return View(entregaEquipo);
-            }
         }
     }
+}
 
